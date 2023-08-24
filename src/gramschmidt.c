7439aacd2b48ec3fb437eeb103f73e7b00d22dd3
Sampie <sfamboni@gmail.com>
2023-08-24 14:29:51 -0300

    paralelismo com pthreads concluido

--- src/gramschmidt.c
+++ src/gramschmidt.c
@@ -14,6 +14,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <stdint.h>
 
 /* Include polybench common header. */
 #include "polybench.h"
@@ -28,49 +29,70 @@ typedef struct {
 
 typedef struct {
   int num_threads;
-  int seed;
+  uint32_t seed;
   char tamanho;
 } Programa;
 
+static int M, N;
+
 /* Array initialization. */
-static void init_array(int m, int n, DATA_TYPE POLYBENCH_2D(A, M, N, m, n),
-                       DATA_TYPE POLYBENCH_2D(R, N, N, n, n),
-                       DATA_TYPE POLYBENCH_2D(Q, M, N, m, n));
+static void init_array(uint32_t seed, int m, int n);
 
 /* DCE code. Must scan the entire live-out data.
    Can be used also to check the correctness of the output. */
-static void print_array(int m, int n, DATA_TYPE POLYBENCH_2D(A, M, N, m, n),
-                        DATA_TYPE POLYBENCH_2D(R, N, N, n, n),
-                        DATA_TYPE POLYBENCH_2D(Q, M, N, m, n));
+static void print_array(int m, int n, double *A, double *R, double *Q);
 
 /* Main computational kernel. The whole function will be timed,
    including the call and return. */
 /* QR Decomposition with Modified Gram Schmidt:
  http://www.inf.ethz.ch/personal/gander/ */
-static void kernel_gramschmidt(int m, int n,
-                               DATA_TYPE POLYBENCH_2D(A, M, N, m, n),
-                               DATA_TYPE POLYBENCH_2D(R, N, N, n, n),
-                               DATA_TYPE POLYBENCH_2D(Q, M, N, m, n));
+static void kernel_gramschmidt(int m, int n, double *A, double *R, double *Q);
 
 static void definir_programa(Programa *programa, int argc, char *argv[]);
 static void print_ajuda(void);
 static void *gramschmidt_par(void *arg);
-static void chamar_gs_seq(int argc, char *argv[]);
-static void chamar_gs_par(int num_threads, int argc, char *argv[]);
+static void chamar_gs_par(int num_threads);
 
 static pthread_spinlock_t sl;
 static pthread_barrier_t barreira;
 static pthread_barrier_t barreira2;
 
+static double *A, *R, *Q;
+
 int main(int argc, char **argv) {
   Programa programa = {0};
   definir_programa(&programa, argc, argv);
 
+  /* Retrieve problem size. */
+  int m = M;
+  int n = N;
+
+  /* Initialize array(s). */
+  init_array(programa.seed, m, n);
+
+  /* Start timer. */
+  /* polybench_start_instruments; */
+
   if (programa.num_threads == 1) {
-    chamar_gs_seq(argc, argv);
+    /* Run kernel. */
+    kernel_gramschmidt(m, n, A, R, Q);
   } else {
-    chamar_gs_par(programa.num_threads, argc, argv);
+    chamar_gs_par(programa.num_threads);
   }
+
+  /* Stop and print timer. */
+  /* polybench_stop_instruments; */
+  /* polybench_print_instruments; */
+
+  /* Prevent dead-code elimination. All live-out data must be printed
+     by the function call in argument. */
+  /* polybench_prevent_dce(print_array(m, n, POLYBENCH_ARRAY(A), */
+  /*                                   POLYBENCH_ARRAY(R), POLYBENCH_ARRAY(Q))); */
+
+  /* Be clean. */
+  /* POLYBENCH_FREE_ARRAY(A); */
+  /* POLYBENCH_FREE_ARRAY(R); */
+  /* POLYBENCH_FREE_ARRAY(Q); */
 
   return 0;
 }
@@ -79,38 +101,50 @@ static double nrm_real;
 
 static void *gramschmidt_par(void *arg) {
   const Args *args = (Args *)arg;
-  int k, i, j;
+  int k, i, j, offsetA, offsetR, offsetQ;
 
   double nrm;
-
-  double R[N][N], Q[M][N], A[M][N];
 
   for (k = 0; k < N; k++) { // Lê todos os vetores
     nrm = SCALAR_VAL(0.0);
     nrm_real = 0.0;
 
-    for (i = args->inicio; i < args->fim; i++) // PARALELIZA AQUI
-      nrm += A[i][k] *
-             A[i][k]; // Normal é a soma dos quadrados de cada elemento do vetor
-
-    pthread_barrier_wait(&barreira);
+    for (i = args->inicio; i < args->fim; i++) { // PARALELIZA AQUI
+      offsetA = i + M * k;
+      nrm += A[offsetA] *
+             A[offsetA]; // Normal é a soma dos quadrados de cada elemento do vetor
+    } 
 
     pthread_spin_lock(&sl);
     nrm_real += nrm;              // BARREIRA E LOCK
-    R[k][k] = SQRT_FUN(nrm_real); // R[k][k] guarda a raiz da normal
     pthread_spin_unlock(&sl);
 
-    for (i = args->inicio; i < args->fim; i++) // PARALELIZA AQUI
-      Q[i][k] = A[i][k] / R[k][k];
+    offsetR = k + N * k;
+    R[offsetR] = SQRT_FUN(nrm_real); // R[k][k] guarda a raiz da normal
+    pthread_barrier_wait(&barreira);
+    for (i = args->inicio; i < args->fim; i++) { // PARALELIZA AQUI
+      offsetA = i + M * k;
+      offsetR = k + N * k;
+      Q[offsetA] = A[offsetA] / R[offsetR];
+    }
 
     for (j = k + 1; j < N; j++) {
-      R[k][j] = SCALAR_VAL(0.0);
+      offsetR = k + N * j;
+      R[offsetR] = SCALAR_VAL(0.0);
 
-      for (i = args->inicio; i < args->fim; i++) // PARALELIZA AQUI
-        R[k][j] += Q[i][k] * A[i][j];
+      for (i = args->inicio; i < args->fim; i++) { // PARALELIZA AQUI
+        offsetA = i + M * j;
+        offsetQ = i + M * k;
+        offsetR = k + N * j;
+        R[offsetR] += Q[offsetQ] * A[offsetA];
+      }
 
-      for (i = args->inicio; i < args->fim; i++) // PARELILZA AQUI
-        A[i][j] = A[i][j] - Q[i][k] * R[k][j];
+      for (i = args->inicio; i < args->fim; i++) { // PARELILZA AQUI
+        offsetA = i + M * j;
+        offsetQ = i + M * k;
+        offsetR = k + N * j;
+        A[offsetA] = A[offsetA] - Q[offsetQ] * R[offsetR];
+      }
     }
     pthread_barrier_wait(&barreira2);
   }
@@ -124,17 +158,20 @@ static void definir_programa(Programa *programa, int argc, char *argv[]) {
     switch (opt) {
     case 'd':
       if (strncmp(optarg, "small", 5) == 0) {
-        programa->tamanho = 's';
+        M = 4000;
+        N = 4800;
       } else if (strncmp(optarg, "medium", 6) == 0) {
-        programa->tamanho = 'm';
+        M = 5000;
+        N = 6000;
       } else if (strncmp(optarg, "large", 5) == 0) {
-        programa->tamanho = 'l';
+        M = 6000;
+        N = 7200;
       } else {
         printf("Tamanho não suportado, use: small, medium ou large\n");
         exit(EXIT_FAILURE);
       }
-
       break;
+
     case 't':
       aux = atoi(optarg);
       if (aux < 1) {
@@ -143,12 +180,12 @@ static void definir_programa(Programa *programa, int argc, char *argv[]) {
       } else {
         programa->num_threads = aux;
       }
-
       break;
+      
     case 'S':
       programa->seed = atoi(optarg);
-
       break;
+
     case 'h':
       print_ajuda();
       exit(EXIT_SUCCESS);
@@ -170,42 +207,7 @@ static void print_ajuda(void) {
       "ajuda.\n");
 }
 
-static void chamar_gs_seq(int argc, char *argv[]) {
-  /* Retrieve problem size. */
-  int m = M;
-  int n = N;
-
-  /* Variable declaration/allocation. */
-  POLYBENCH_2D_ARRAY_DECL(A, DATA_TYPE, M, N, m, n);
-  POLYBENCH_2D_ARRAY_DECL(R, DATA_TYPE, N, N, n, n);
-  POLYBENCH_2D_ARRAY_DECL(Q, DATA_TYPE, M, N, m, n);
-
-  /* Initialize array(s). */
-  init_array(m, n, POLYBENCH_ARRAY(A), POLYBENCH_ARRAY(R), POLYBENCH_ARRAY(Q));
-
-  /* Start timer. */
-  polybench_start_instruments;
-
-  /* Run kernel. */
-  kernel_gramschmidt(m, n, POLYBENCH_ARRAY(A), POLYBENCH_ARRAY(R),
-                     POLYBENCH_ARRAY(Q));
-
-  /* Stop and print timer. */
-  polybench_stop_instruments;
-  polybench_print_instruments;
-
-  /* Prevent dead-code elimination. All live-out data must be printed
-     by the function call in argument. */
-  polybench_prevent_dce(print_array(m, n, POLYBENCH_ARRAY(A),
-                                    POLYBENCH_ARRAY(R), POLYBENCH_ARRAY(Q)));
-
-  /* Be clean. */
-  POLYBENCH_FREE_ARRAY(A);
-  POLYBENCH_FREE_ARRAY(R);
-  POLYBENCH_FREE_ARRAY(Q);
-}
-
-static void chamar_gs_par(int num_threads, int argc, char *argv[]) {
+static void chamar_gs_par(int num_threads) {
   pthread_barrier_init(&barreira, NULL, num_threads);
   pthread_barrier_init(&barreira2, NULL, num_threads);
   pthread_spin_init(&sl, num_threads);
@@ -229,69 +231,82 @@ static void chamar_gs_par(int num_threads, int argc, char *argv[]) {
   pthread_spin_destroy(&sl);
 }
 
-static void init_array(int m, int n, DATA_TYPE POLYBENCH_2D(A, M, N, m, n),
-                       DATA_TYPE POLYBENCH_2D(R, N, N, n, n),
-                       DATA_TYPE POLYBENCH_2D(Q, M, N, m, n)) {
-  int i, j;
+static void init_array(uint32_t seed, int m, int n) {
+  A = (double *)malloc(M * N * sizeof(double));
+  R = (double *)malloc(N * N * sizeof(double));
+  Q = (double *)malloc(M * N * sizeof(double));
+  int i, j, offset;
+  srand(seed);
 
   for (i = 0; i < m; i++)
     for (j = 0; j < n; j++) {
-      A[i][j] = (((DATA_TYPE)((i * j) % m) / m) * 100) + 10;
-      Q[i][j] = 0.0;
+      offset = i + M * j;
+      A[offset] = ((float)rand()/(float)RAND_MAX) * 1000;
+      Q[offset] = 0.0;
     }
   for (i = 0; i < n; i++)
     for (j = 0; j < n; j++)
-      R[i][j] = 0.0;
+      offset = i + M * j;
+      R[offset] = 0.0;
 }
 
-static void print_array(int m, int n, DATA_TYPE POLYBENCH_2D(A, M, N, m, n),
-                        DATA_TYPE POLYBENCH_2D(R, N, N, n, n),
-                        DATA_TYPE POLYBENCH_2D(Q, M, N, m, n)) {
-  int i, j;
+/* static void print_array(int m, int n, double *A, double *R, double *Q) { */
+  /* int i, j; */
 
-  POLYBENCH_DUMP_START;
-  POLYBENCH_DUMP_BEGIN("R");
-  for (i = 0; i < n; i++)
-    for (j = 0; j < n; j++) {
-      if ((i * n + j) % 20 == 0)
-        fprintf(POLYBENCH_DUMP_TARGET, "\n");
-      fprintf(POLYBENCH_DUMP_TARGET, DATA_PRINTF_MODIFIER, R[i][j]);
-    }
-  POLYBENCH_DUMP_END("R");
+  /* /1* POLYBENCH_DUMP_START; *1/ */
+  /* /1* POLYBENCH_DUMP_BEGIN("R"); *1/ */
+  /* for (i = 0; i < n; i++) */
+  /*   for (j = 0; j < n; j++) { */
+  /*     if ((i * n + j) % 20 == 0) */
+  /*       fprintf(POLYBENCH_DUMP_TARGET, "\n"); */
+  /*     fprintf(POLYBENCH_DUMP_TARGET, DATA_PRINTF_MODIFIER, R[i][j]); */
+  /*   } */
+  /* /1* POLYBENCH_DUMP_END("R"); *1/ */
 
-  POLYBENCH_DUMP_BEGIN("Q");
-  for (i = 0; i < m; i++)
-    for (j = 0; j < n; j++) {
-      if ((i * n + j) % 20 == 0)
-        fprintf(POLYBENCH_DUMP_TARGET, "\n");
-      fprintf(POLYBENCH_DUMP_TARGET, DATA_PRINTF_MODIFIER, Q[i][j]);
-    }
-  POLYBENCH_DUMP_END("Q");
-  POLYBENCH_DUMP_FINISH;
-}
+  /* /1* POLYBENCH_DUMP_BEGIN("Q"); *1/ */
+  /* for (i = 0; i < m; i++) */
+  /*   for (j = 0; j < n; j++) { */
+  /*     if ((i * n + j) % 20 == 0) */
+  /*       fprintf(POLYBENCH_DUMP_TARGET, "\n"); */
+  /*     fprintf(POLYBENCH_DUMP_TARGET, DATA_PRINTF_MODIFIER, Q[i][j]); */
+  /*   } */
+  /* /1* POLYBENCH_DUMP_END("Q"); *1/ */
+  /* /1* POLYBENCH_DUMP_FINISH; *1/ */
+/* } */
 
-static void kernel_gramschmidt(int m, int n,
-                               DATA_TYPE POLYBENCH_2D(A, M, N, m, n),
-                               DATA_TYPE POLYBENCH_2D(R, N, N, n, n),
-                               DATA_TYPE POLYBENCH_2D(Q, M, N, m, n)) {
-  int i, j, k;
+static void kernel_gramschmidt(int m, int n, double *A, double *R, double *Q) {
+  int i, j, k, offsetA, offsetR, offsetQ;
 
   DATA_TYPE nrm;
 
 #pragma scop
   for (k = 0; k < _PB_N; k++) {
     nrm = SCALAR_VAL(0.0);
-    for (i = 0; i < _PB_M; i++)
-      nrm += A[i][k] * A[i][k];
-    R[k][k] = SQRT_FUN(nrm);
-    for (i = 0; i < _PB_M; i++)
-      Q[i][k] = A[i][k] / R[k][k];
+    for (i = 0; i < _PB_M; i++) {
+      offsetA = i + M * k;
+      nrm += A[offsetA] * A[offsetA];
+    }
+    R[k + M * k] = SQRT_FUN(nrm);
+    for (i = 0; i < _PB_M; i++) {
+      offsetA = i + M * k;
+      offsetR = k + M * k;
+      Q[offsetA] = A[offsetA] / R[offsetR];
+    }
     for (j = k + 1; j < _PB_N; j++) {
-      R[k][j] = SCALAR_VAL(0.0);
-      for (i = 0; i < _PB_M; i++)
-        R[k][j] += Q[i][k] * A[i][j];
-      for (i = 0; i < _PB_M; i++)
-        A[i][j] = A[i][j] - Q[i][k] * R[k][j];
+      offsetR = k + M * j;
+      R[offsetR] = SCALAR_VAL(0.0);
+      for (i = 0; i < _PB_M; i++) {
+        offsetA = i + M * j;
+        offsetQ = i + M * k;
+        offsetR = k + M * j;
+        R[offsetR] += Q[offsetQ] * A[offsetA];
+      }
+      for (i = 0; i < _PB_M; i++) {
+        offsetA = i + M * j;
+        offsetQ = i + M * k;
+        offsetR = k + M * j;
+        A[offsetA] = A[offsetA] - Q[offsetQ] * R[offsetR];
+      }
     }
   }
 #pragma endscop
--- src/gramschmidt.h
+++ src/gramschmidt.h
@@ -11,40 +11,40 @@
 #define _GRAMSCHMIDT_H
 
 /* Default to LARGE_DATASET. */
-#if !defined(MINI_DATASET) && !defined(SMALL_DATASET) &&                       \
-    !defined(MEDIUM_DATASET) && !defined(LARGE_DATASET) &&                     \
-    !defined(EXTRALARGE_DATASET)
-#define LARGE_DATASET
-#endif
+/* #if !defined(MINI_DATASET) && !defined(SMALL_DATASET) &&                       \ */
+/*     !defined(MEDIUM_DATASET) && !defined(LARGE_DATASET) &&                     \ */
+/*     !defined(EXTRALARGE_DATASET) */
+/* #define LARGE_DATASET */
+/* #endif */
 
-#if !defined(M) && !defined(N)
-/* Define sample dataset sizes. */
-#ifdef MINI_DATASET
-#define M 20
-#define N 30
-#endif
+/* #if !defined(M) && !defined(N) */
+/* /1* Define sample dataset sizes. *1/ */
+/* #ifdef MINI_DATASET */
+/* #define M 20 */
+/* #define N 30 */
+/* #endif */
 
-#ifdef SMALL_DATASET
-#define M 60
-#define N 80
-#endif
+/* #ifdef SMALL_DATASET */
+/* #define M 60 */
+/* #define N 80 */
+/* #endif */
 
-#ifdef MEDIUM_DATASET
-#define M 200
-#define N 240
-#endif
+/* #ifdef MEDIUM_DATASET */
+/* #define M 200 */
+/* #define N 240 */
+/* #endif */
 
-#ifdef LARGE_DATASET
-#define M 1000
-#define N 1200
-#endif
+/* #ifdef LARGE_DATASET */
+/* #define M 1000 */
+/* #define N 1200 */
+/* #endif */
 
-#ifdef EXTRALARGE_DATASET
-#define M 2000
-#define N 2600
-#endif
+/* #ifdef EXTRALARGE_DATASET */
+/* #define M 2000 */
+/* #define N 2600 */
+/* #endif */
 
-#endif /* !(M N) */
+/* #endif /1* !(M N) *1/ */
 
 #define _PB_M POLYBENCH_LOOP_BOUND(M, m)
 #define _PB_N POLYBENCH_LOOP_BOUND(N, n)
