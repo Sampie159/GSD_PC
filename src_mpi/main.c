9d8f696381ef699be60dcafb2c31fac9278fb754
Sampie159 <38163547+Sampie159@users.noreply.github.com>
2023-09-21 23:26:36 -0300

    programa roda, porém péssima performance

--- src_mpi/main.c
+++ src_mpi/main.c
@@ -6,24 +6,21 @@
 #include <string.h>
 #include <unistd.h>
 
-// TODO: Melhorar definição de Slice
-typedef struct {
-  int     tamanho;
-  double *vetor;
-} Slice;
-
 // variáveis
 static uint32_t seed;
 static double  *A, *R, *Q;
 static int      M, N;
+static int      tamanho_sub_arranjo;
+static int      tamanho_global, rank_global;
+static int      linhas_processo;
+static double  *vetor;
 
 // Funções
-static void gramschmidt_mpi(const int *inicio, const int *fim,
-                            const int *tamanho_global, const int *rank_global,
-                            const int *linhas_processo);
+static void gramschmidt_mpi(const int *inicio, const int *fim);
 static void ler_args(int *argc, char ***argv);
 static void preencher_matriz(void);
 static void print_ajuda(void);
+static void atualizar_matrizes(void);
 
 int
 main(int argc, char **argv) {
@@ -31,18 +28,19 @@ main(int argc, char **argv) {
   ler_args(&argc, &argv);
   preencher_matriz();
 
-  int tamanho_global, rank_global;
-
   MPI_Init(&argc, &argv);
   MPI_Comm_size(MPI_COMM_WORLD, &tamanho_global);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank_global);
 
-  int linhas_processo = M / tamanho_global;
-  int inicio          = rank_global * linhas_processo;
+  linhas_processo = M / tamanho_global;
+  int inicio      = rank_global * linhas_processo;
   int fim = (rank_global == tamanho_global - 1) ? M : inicio + linhas_processo;
 
-  gramschmidt_mpi(&inicio, &fim, &tamanho_global, &rank_global,
-                  &linhas_processo);
+  tamanho_sub_arranjo = linhas_processo * N;
+
+  vetor = (double *) malloc(sizeof(double) * tamanho_sub_arranjo);
+
+  gramschmidt_mpi(&inicio, &fim);
 
   MPI_Finalize();
 
@@ -50,25 +48,25 @@ main(int argc, char **argv) {
 }
 
 static void
-gramschmidt_mpi(const int *inicio, const int *fim, const int *tamanho_global,
-                const int *rank_global, const int *linhas_processo) {
+gramschmidt_mpi(const int *inicio, const int *fim) {
   double nrm, raiz_nrm;
   int    i, j, k, offsetA, offsetR, offsetQ;
 
   for (k = 0; k < N; k++) {
-    nrm = 0;
+    nrm      = 0;
+    raiz_nrm = 0;
 
     for (i = *inicio; i < *fim; i++) {
       offsetA  = i + M * k;
       nrm     += A[offsetA] * A[offsetA];
       if (rank_global > 0) {
         double nrm_aux;
-        MPI_Recv(&nrm_aux, 1, MPI_DOUBLE, *rank_global - 1, 0, MPI_COMM_WORLD,
+        MPI_Recv(&nrm_aux, 1, MPI_DOUBLE, rank_global - 1, 0, MPI_COMM_WORLD,
                  MPI_STATUS_IGNORE);
         nrm += nrm_aux;
       }
       if (rank_global < tamanho_global - 1) {
-        MPI_Send(&nrm, 1, MPI_DOUBLE, *rank_global + 1, 0, MPI_COMM_WORLD);
+        MPI_Send(&nrm, 1, MPI_DOUBLE, rank_global + 1, 0, MPI_COMM_WORLD);
       }
     }
 
@@ -76,7 +74,7 @@ gramschmidt_mpi(const int *inicio, const int *fim, const int *tamanho_global,
       raiz_nrm = sqrt(nrm);
     }
 
-    MPI_Bcast(&raiz_nrm, 1, MPI_DOUBLE, *tamanho_global - 1, MPI_COMM_WORLD);
+    MPI_Bcast(&raiz_nrm, 1, MPI_DOUBLE, tamanho_global - 1, MPI_COMM_WORLD);
     offsetR    = k + N * k;
     R[offsetR] = raiz_nrm;
 
@@ -101,9 +99,8 @@ gramschmidt_mpi(const int *inicio, const int *fim, const int *tamanho_global,
         A[offsetQ] -= R[offsetR] * Q[offsetA];
       }
     }
-
-    if (rank_global > 0) {
-      // TODO: Enviar slice de A para o próximo processo
+    if (tamanho_global != 1) {
+      atualizar_matrizes();
     }
   }
 }
@@ -160,4 +157,24 @@ print_ajuda(void) {
       "  -S <seed>                Semente para o gerador de números "
       "aleatórios\n"
       "  -h                       Exibe esta mensagem de ajuda\n");
+}
+
+static void
+atualizar_matrizes(void) {
+  int offset;
+  if (rank_global < tamanho_global - 1) {
+    offset = rank_global * linhas_processo * N;
+    memcpy(vetor, A + offset, tamanho_sub_arranjo);
+    MPI_Send(vetor, tamanho_sub_arranjo, MPI_DOUBLE, tamanho_global - 1, 0,
+             MPI_COMM_WORLD);
+  } else if (rank_global == tamanho_global - 1) {
+    for (int i = 0; i < rank_global; i++) {
+      offset = i * linhas_processo * N;
+      MPI_Recv(vetor, tamanho_sub_arranjo, MPI_DOUBLE, i, 0, MPI_COMM_WORLD,
+               MPI_STATUS_IGNORE);
+      memcpy(A + offset, vetor, tamanho_sub_arranjo);
+    }
+  }
+
+  MPI_Bcast(A, M * N, MPI_DOUBLE, tamanho_global - 1, MPI_COMM_WORLD);
 }
